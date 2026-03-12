dc62f3e903176a16ef8ef3bd1af58973efcdc6c0
projektdenis <deniskrosniqi@gmail.com>
2026-03-12 13:12:44 +0100

    hidden uri

--- server.js
+++ server.js
@@ -1,26 +1,32 @@
+
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import router from "./routes/contact.js"
+import router from "./routes/contact.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-
 const PORT = 5000;
 
 app.use(express.json());
 app.use(cors());
 
+const mongoUri = process.env.MONGODB_URI;
 
-try {
-    const conn = await mongoose.connect("mongodb+srv://denis:nIp6MREclyw9bECS@mern.5dgxw.mongodb.net/sample_mflix?retryWrites=true&w=majority&appName=MERN");
-    console.log(`MongoDb Connected: ${conn.connection.host}`);
-} catch (error) {
-    console.error(`${error}`);
-    process.exit(1);
+async function startServer() {
+    try {
+        const conn = await mongoose.connect(mongoUri);
+        console.log(`MongoDb Connected: ${conn.connection.host}`);
+        app.use("/api/user", router);
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.error(`${error}`);
+        process.exit(1);
+    }
 }
 
-app.use("/api/user", router);
-
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+startServer();
